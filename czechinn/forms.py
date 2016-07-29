1e47e46b03f72521d0000472b8f14bb4bfcddd37
Roderic Day <roderic.day@gmail.com>
2016-07-29 14:23:01 -0700

    Change form order

--- czechinn/forms.py
+++ czechinn/forms.py
@@ -13,17 +13,17 @@ class TransactionForm(forms.Form):
 class PatientForm(forms.Form):
     GENDER = [('Other', 'Other'), ('Male', 'Male'), ('Female', 'Female')]
 
-    date_of_birth = forms.DateField()
-    doctor = forms.ChoiceField(choices=DOCTORS)
-    gender = forms.ChoiceField(choices=GENDER)
     first_name = forms.CharField()
     last_name = forms.CharField()
+    gender = forms.ChoiceField(choices=GENDER)
+    date_of_birth = forms.DateField()
+    doctor = forms.ChoiceField(choices=DOCTORS)
 
 
 class AppointmentForm(forms.Form):
     # the patient field set by transactional
     doctor = forms.ChoiceField(choices=DOCTORS)
-    duration = forms.ChoiceField(choices=[(15, '15m'), (30, '30m')])  # Duration field gives 00:00?
+    duration = forms.ChoiceField(choices=[(15, '15m'), (30, '30m')])
     exam_room = forms.ChoiceField(choices=[(1,'Red'), (2, 'Blue')])
     office = forms.ChoiceField(choices=[(109278, 'Rm1')])
     scheduled_time = forms.DateTimeField(initial=datetime.datetime.now)
